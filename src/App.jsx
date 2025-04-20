90b935d5a7071399d81fca21fa13438c9b6ea5d3
Phani <mysteriousphani@gmail.com>
2025-04-20 15:29:43 +0530

    resolve dependencies

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ function App() {
   return (
     <div className='h-screen flex items-start py-10 justify-center bg-violet-100'>
       <TaskTracker />
+      <button >Dark Mode</button>
     </div>
   )
 }
